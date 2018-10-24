ce532a04e68fa6553200392288f116d442ae92c2
FND <fnd@localhost.localdomain>
2018-10-24 11:09:19 +0200

    simplified release distribution
    we don't want Yet Another Config File

--- faucet.config.js
+++ faucet.config.js
@@ -1,9 +1,16 @@
 "use strict";
 
+let bundle = {
+	source: "./src/index.js",
+	target: "./dist/bundle.js"
+};
+
+if(process.env.HIJAX_DIST) {
+	bundle.target = "./hijax-form.js";
+	bundle.esnext = true;
+}
+
 module.exports = {
 	watchDirs: ["./src", "./test"],
-	js: [{
-		source: "./src/index.js",
-		target: "./dist/bundle.js"
-	}]
+	js: [bundle]
 };
